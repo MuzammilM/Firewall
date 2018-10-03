2a0657eb37793c5f7d2f0597cf85c24ac599fa5b
Muzammil Hasan Momin <muzammilmomin@live.com>
2018-10-03 14:18:24 +0530

    Create firewall.sh

---
+++ firewall.sh
@@ -0,0 +1,10 @@
+sudo apt-get install iptables -y
+sudo iptables -A INPUT -i lo -m comment --comment "Allow loopback connections" -j ACCEPT
+sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -j DROP
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+sudo apt-get -y install iptables-persistent
+sudo apt-get install fail2ban -y
+sudo service fail2ban restart
